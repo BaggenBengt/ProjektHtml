867e30ff79a12fa45ff38c691cdfd827157c0b93
BaggenBengt <47028924+BaggenBengt@users.noreply.github.com>
2019-09-19 18:15:48 +0200

    12

---
+++ ProjektJavascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+   
+
+    $("#overlaySpotify").click(function(){
+        $("#spotifyText").fadeIn(1500);
+        })
+        $("#overlayChrome").click(function(){
+            $("#chromeText").fadeIn(1500);
+            })
+
+
+
+
+      });
+
