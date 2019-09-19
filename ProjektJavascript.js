9e75d70a6c3887115bb35e5ee91eed36a03feb50
BaggenBengt <47028924+BaggenBengt@users.noreply.github.com>
2019-09-19 18:48:02 +0200

    12

--- ProjektJavascript.js
+++ ProjektJavascript.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $("#overlaySpotify").click(function(){
         $("#spotifyText").fadeIn(1500);
         })
-        $("#overlayChrome").click(function(){
+        $("#overlayChrome").click(function(){       // När man trycker på logon kommer texten fram
             $("#chromeText").fadeIn(1500);
             })
 
