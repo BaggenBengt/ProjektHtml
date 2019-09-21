e2e9cb288847499235e409bf22ad07e53ee7ae86
mhalmkrona <55440195+mhalmkrona@users.noreply.github.com>
2019-09-21 08:27:35 +0200

    f

--- ProjektJavascript.js
+++ ProjektJavascript.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $('#namn').keyup(funtion(){
-        
-    })
+    var $regexname=/^([a-zA-Z]{3,16})$/;
+    $('#name').on('keypress keydown keyup',function(){
+             if (!$(this).val().match($regexname)) {
+              
+                 $('#spanName').removeClass('hideForm');
+                 $('#spanName').show();
+             }
+           else{
+                
+                $('#spanName').addClass('hideForm');
+               }
+         });
 });
