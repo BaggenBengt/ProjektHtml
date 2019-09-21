b52096af549544937d055ecb1348bd1e5ae0aa51
mhalmkrona <55440195+mhalmkrona@users.noreply.github.com>
2019-09-21 13:34:24 +0200

    yas

--- ProjektJavascript.js
+++ ProjektJavascript.js
@@ -1,14 +1,90 @@
-$(document).ready(function(){
-    var $regexname=/^([a-zA-Z]{3,16})$/;
-    $('#name').on('keypress keydown keyup',function(){
-             if (!$(this).val().match($regexname)) {
-              
-                 $('#spanName').removeClass('hideForm');
-                 $('#spanName').show();
-             }
-           else{
-                
-                $('#spanName').addClass('hideForm');
-               }
-         });
+
+$(document).on('change',function(){
+    validEmail();
+    validNamn();
+    //validTel();
+    submitToggle();
+    
 });
+
+
+function formValidate(){
+
+    var validForm = false;
+    
+    if(validNamn() && validEmail() && validTel() && validMeddelande()){
+        validForm = true;
+    }
+    else {
+        validForm = false;
+    }
+    return validForm;
+}
+
+function submitToggle(){
+    if(formValidate()){
+        $('#submit').removeClass('disabled');
+    }
+    else {
+        $('#submit').addClass('disabled');
+    }
+}
+
+function validNamn(){
+
+    $('#namn').keyup(function(){
+        var valid= false;
+              
+ if(!/^(\w+\S+)$/.test($(this).val()) || $(this).val().length < 3 ){
+     valid = false;
+     $('#spanNamn').removeClass('hideForm');
+     }
+ else{
+     $('#spanNamn').addClass('hideForm');
+     valid = true;
+ }
+ return valid;
+ });
+}
+
+function validEmail(){
+
+    $('#email').keyup(function(){
+           var valid= false;
+           
+           
+    if(!/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test($(this).val())){
+        valid = false;
+        $('#spanEmail').removeClass('hideForm');
+        }
+    else{
+        $('#spanEmail').addClass('hideForm');
+        valid = true;
+    }
+    return valid;
+    });
+}
+
+
+/*
+function validTel(){
+    $('#tel').keyup(function(){
+        var valid= false;
+        
+        
+ if(!//.test($(this).val())){
+     valid = false;
+     $('#spanTel').removeClass('hideForm');
+     }
+ else{
+     $('#spanTel').addClass('hideForm');
+     valid = true;
+ }
+ return valid;
+ });
+}*/
+
+function validMeddelande(){
+    
+
+    }
