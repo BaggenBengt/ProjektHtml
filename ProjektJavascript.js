8a22271c83a51dda873779897851bb6ea5169250
BaggenBengt <47028924+BaggenBengt@users.noreply.github.com>
2019-09-24 11:58:38 +0200

    124124

--- ProjektJavascript.js
+++ ProjektJavascript.js
@@ -1,20 +1,44 @@
 $(document).ready(function(){
-        
+         
     $("#robin").click(function(){
+    $("#robin").removeClass("fornamn"); 
+    $("#mattias, #oskar, #alla").removeClass("selected");
+    $("#mattias, #oskar, #alla").addClass("fornamn")
+     $("#robin").addClass("selected");   
      $("#bottom").empty();
      $("#bottom").load("https://raw.githubusercontent.com/BaggenBengt/ProjektHtml/Robin/RobinPersonliga.html");     
     }) 
 
     $("#mattias").click(function(){
         $("#bottom").empty();
+        $("#mattias").removeClass("fornamn"); 
+        $("#robin, #oskar, #alla").removeClass("selected");
+        $("#robin, #oskar, #alla").addClass("fornamn")
+         $("#mattias").addClass("selected"); 
         $("#bottom").load("https://raw.githubusercontent.com/BaggenBengt/ProjektHtml/Robin/MattiasPersonliga.html");     
        }) 
    
        $("#oskar").click(function(){
         $("#bottom").empty();
+        $("#oskar").removeClass("fornamn"); 
+        $("#mattias, #robin, #alla").removeClass("selected");
+        $("#mattias, #robin, #alla").addClass("fornamn")
+         $("#oskar").addClass("selected"); 
         $("#bottom").load("https://raw.githubusercontent.com/BaggenBengt/ProjektHtml/Robin/OskarPersonliga.html");     
        }) 
 
+
+       $("#alla").click(function(){
+        $("#alla").removeClass("fornamn"); 
+        $("#mattias, #robin, #oskar").removeClass("selected");
+        $("#mattias, #robin, #oskar").addClass("fornamn")
+         $("#alla").addClass("selected"); 
+         $("#bakgrund").load("");
+
+
+
+
+       })
 
       
 
