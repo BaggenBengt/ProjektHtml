67910aecc15aa65155a1c7a566ba9531dffd20d0
oskareriksson123 <47111141+oskareriksson123@users.noreply.github.com>
2019-09-26 11:26:17 +0200

    Helt DONE!

--- ProjektJavascript.js
+++ ProjektJavascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
+   
     
-
     $("#robin").click(function(){                          
     $("#robin").removeClass("fornamn");                     //Funktion för att få understruket namn
     $("#mattias, #oskar, #alla").removeClass("selected");   // och loada ny profil på "Om oss" sidan
@@ -48,10 +48,12 @@ $(document).ready(function(){
 
 
 //Startar och stänger av Timern anropar även funktionen "bytBild()" ----> Knappen "spela bildspel har value " " för att inget ska synas på knappen
+// Startar även musiken!
 
 $('#spelaBildspel').click(function(){
     var namn = $('#spelaBildspel').val();
     if (namn == " "){
+        playAudio()
     tid = setInterval("bytBild()", 1000);
 
     $('#spelaBildspel').val("  ");
@@ -59,6 +61,7 @@ $('#spelaBildspel').click(function(){
     $("#spelaBildspel").css("background-image", "url('pause1.png')");
 }
      if (namn == "  "){
+        x.pause();
     clearInterval(tid);
     $('#spelaBildspel').val(" ");
     $('#spelaBildspel').css('opacity', '1');
@@ -67,6 +70,18 @@ $('#spelaBildspel').click(function(){
     });
     //Startar och stänger av Timern <----
 });
+
+//Musiken till bildspelet!
+var x = document.getElementById("musikSpelare");
+
+function playAudio() {
+  x.play();
+}
+function pauseAudio() {
+  x.pause();
+}
+
+
 
 
 
@@ -82,23 +97,38 @@ burger.addEventListener('click',()=>{
 }
 navSlide();
 
+//Gömmer Bildtexten  och spela upp knappen när hamburgaren åker ut!
+
+$(".scrollLista").click(function(){
+    setTimeout(function(){
+
+    $("#textBildspel").toggle();
+    $("#spelaBildspel").toggle();
+    }, 500);
+  });
+
 
 // Funktionen som byter själva bilden!
 function bytBild(){
    
     var image = document.getElementById("imgBildSpel");
+    var x = document.getElementById("textBildspel");
     if(image.src.match("bild1.jpg")){
     image.src = "bild2.jpg";
+    x.innerHTML ="Till"
   
     }
     else if (image.src.match("bild2.jpg")){
     image.src = "bild3.jpg";
+    x.innerHTML="Våran"
     }
     else if (image.src.match("bild3.jpg")){
     image.src = "bild4.jpg";
+    x.innerHTML="Portfolio"
     }
     else{
     image.src = "bild1.jpg";
+    x.innerHTML="Välkommen"
     }
     }
 
